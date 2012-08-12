b33a819ca5429264a652341b1a5f3f6ebd19775f
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-08-12 12:09:40 -0300

    Playing around with the sample spec.

--- src/test/scala/net/rafaelferreira/AddressMapperSpec.scala
+++ src/test/scala/net/rafaelferreira/AddressMapperSpec.scala
@@ -10,13 +10,13 @@ class AddressMapperSpec extends Specification with Goose {
     _.when(id ==> "123").
       and(database.stub(_.find("addresses", "123")) ==> Some(Map("city" -> "789", "street" -> "999"))).
       and(database.stub(_.find("cities", "789")) ==> Some(Map("name" -> "Curitiba"))).
-      but {
+      and(database.stub(_.find("streets", "999")) ==> Some(Map("name" -> "St. st."))).
+      then(_ must_== Some(Address(City("Curitiba"), Street("St. st.")))).but {
         _.when(database.stub(_.find("streets", "999")) ==>  None).
-        then(_ must_== None)
-      }.
-      but {
-        _.when(database.stub(_.find("streets", "999")) ==> Some(Map("name" -> "St. st."))).
-        then(_ must_== Some(Address(City("Curitiba"), Street("St. st."))))
+          then(_ must_== None)
+      }.but {
+        _.when(database.stub(_.find("cities", "789")) ==> None).
+          then(_ must beNone)
       }
   }
 }
