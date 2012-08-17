e8f4cacff831e7ddd23bf724b54e33f97eb156cb
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-08-16 23:39:44 -0300

    More tests.

--- src/test/scala/net/rafaelferreira/goose/GooseSpec.scala
+++ src/test/scala/net/rafaelferreira/goose/GooseSpec.scala
@@ -6,16 +6,19 @@ import org.specs2.execute._
 
 
 class GooseSpec extends Specification with ResultMatchers with Goose {
-  def is = `variable assumption passing` ^ `variable assumption failing`
+  def is = "Specifying goose itself" ^
+            `variable assumption passing` ^
+            `variable assumption failing` ^ 
+            `stub assumption passing`
   
-  def `variable assumption passing` =
+  def `variable assumption passing` = "variable assumption passing" ^
     check({(x:String,y:String) => x+y}) {(value, y) => 
       _.when(value ==> "as").
-        when(y ==> "df").
+        and(y ==> "df").
         then(_ must_== "asdf")
     }
-
-  def `variable assumption failing` = {
+  
+  def `variable assumption failing` = "variable assumption failing" ^{
     val fragments = check({ (x: String, y: String) => x + y }) { (value, y) =>
       _.when(value ==> "xx").
         when(y ==> "xx").
@@ -23,4 +26,17 @@ class GooseSpec extends Specification with ResultMatchers with Goose {
     }
     fragments.examples.head.execute must be failing
   }
+    
+  def `stub assumption passing` = "stub assumption passing" ^{
+    trait Foo {def foo:String}
+    trait Bar {def bar:String}
+    
+    check((_:Foo).foo + (_:Bar).bar) {(foo, bar) => 
+      _.when(foo.stub(_.foo) ==> "as").
+        and(bar.stub(_.bar) ==> "df").
+        then(_ must_== "asdf")
+    }
+  }
+
+
 }
