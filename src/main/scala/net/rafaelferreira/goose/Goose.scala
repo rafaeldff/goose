520595bf24955c1e0550a95da0b9e4940065023c
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-08-19 18:08:42 -0300

    Extracting the 'check' methods to their own trait

--- src/main/scala/net/rafaelferreira/goose/Goose.scala
+++ src/main/scala/net/rafaelferreira/goose/Goose.scala
@@ -10,7 +10,7 @@ import org.specs2.Specification
 import org.specs2.matcher.MatchResult
 import org.specs2.specification.Fragment
 
-trait Goose {this: Specification =>
+trait GooseStructure {this: Specification =>
   import scala.collection.immutable.Map
   
   val mocker = new org.specs2.mock.MockitoMocker {}
@@ -100,29 +100,7 @@ trait Goose {this: Specification =>
   
   def dep[T: ClassManifest]: Dependency[T] = new Dependency[T]
   
-  def check[T1: ClassManifest, R](resultExpression: (T1) => R)(c: (Dependency[T1]) => When[R] => When[R]): Fragments = {
-    val dep1 = dep[T1]
-    val when = new When[R]()((state:State) => resultExpression(state.get(dep1).get))
-    c(dep1)(when).results ^ end
-  }
-  
-  def check[T1: ClassManifest, T2: ClassManifest, R](resultExpression: (T1, T2) => R)(c: (Dependency[T1], Dependency[T2]) => When[R] => When[R]): Fragments = {
-    val dep1 = dep[T1]
-    val dep2 = dep[T2]
-    val when = new When[R]()((state:State) => resultExpression(state.get(dep1).get, state.get(dep2).get))
-    c(dep1, dep2)(when).results ^ end
-  }
-  
-  def check[T1: ClassManifest, T2: ClassManifest, T3: ClassManifest, R](resultExpression: (T1, T2, T3) => R)(c: (Dependency[T1], Dependency[T2], Dependency[T3]) => When[R] => When[R]): Fragments = {
-    val dep1 = dep[T1]
-    val dep2 = dep[T2]
-    val dep3 = dep[T3]
-    val when = new When[R]()((state:State) => resultExpression(state.get(dep1).get, state.get(dep2).get, state.get(dep3).get))
-    c(dep1, dep2, dep3)(when).results ^ end
-  }
 }
 
 
-
-
-
+trait Goose extends GooseStructure with CheckingForVariousArities {self: Specification =>  }
