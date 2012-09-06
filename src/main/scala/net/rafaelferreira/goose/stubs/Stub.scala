34bdc660cdd406fb7c7c82cbfe0cb3492fee95c1
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-09-06 12:11:12 -0300

    Basic stub expectation recording

--- src/main/scala/net/rafaelferreira/goose/stubs/Stub.scala
+++ src/main/scala/net/rafaelferreira/goose/stubs/Stub.scala
@@ -1,7 +1,63 @@
 package net.rafaelferreira.goose
 package stubs
 
-class Stub[T: ClassManifest] {
-    def expecting[R](call: T => R, result: R):Stub[T] = this
-    def stubObject: T = sys.error("todo ")
+import java.lang.reflect.{Method, InvocationHandler}
+
+
+case class Stub[T: ClassManifest](expectations: Seq[Expectation[T]] = Vector()) {
+  lazy val results = 
+    expectations.foldLeft(Map[String,AnyRef]()) {(map, expectation) =>
+      map + (expectation.methodCalled -> expectation.result) 
+    }
+  
+  def expecting[R](expectation:Expectation[T]):Stub[T] = copy(expectations = expectations :+ expectation) 
+  def stubObject: T = ProxyFactory(new InvocationHandler {
+    def invoke(obj:Object, method:Method, args:Array[Object]) = {
+      results(method.getName)
+    }
+  }) 
+}
+
+case class Expectation[T:ClassManifest](call: T => Any, result: AnyRef) {
+  val methodCalled = {
+    val recorder = new Recorder[T]
+    call(recorder())
+    recorder.methodCalled
+  }
+}
+
+class Recorder[T:ClassManifest] {
+  /*
+   * This class encapsulates the mutability inherent in working with proxies 
+   */
+  
+  import java.lang.reflect.{Array=>ReflArray, _}
+  val javaClass = implicitly[ClassManifest[T]].erasure
+  
+  var call: Option[String] = None
+  
+  private val dummy = ProxyFactory(new InvocationHandler {
+    def invoke(obj:Object, method:Method, args:Array[Object]) = {
+      call = Some(method.getName);
+      null
+    }
+  })
+  
+  def apply(): T = dummy
+  def methodCalled:String = call.getOrElse { 
+    throw new IllegalStateException(
+      "Expecting a call on proxy object of class '%s' but none was made." format javaClass.getName
+    ) 
+  } 
+}
+
+object ProxyFactory {
+  import java.lang.reflect.{Array=>_, _}
+  
+  def apply[T:ClassManifest](handler:InvocationHandler) = { 
+    val javaClass = implicitly[ClassManifest[T]].erasure
+    Proxy.
+      newProxyInstance(Thread.currentThread.getContextClassLoader, Array(javaClass), handler).
+      asInstanceOf[T]
+  }
 }
--- src/test/scala/net/rafaelferreira/goose/stubs/StubsSpec.scala
+++ src/test/scala/net/rafaelferreira/goose/stubs/StubsSpec.scala
@@ -3,6 +3,35 @@ package stubs
 
 import org.specs2.Specification
 
-class StubsSpec extends Specification with Goose {
-  def is = ok
+class StubsSpec extends Specification {
+  trait Foo {
+    def foo:String  
+  }
+  
+  def is =  "Stub" ^ p ^
+              "stub no-args method" ! stubNoArgs ^
+            "Recorder" ^ p ^
+              "call recorder records last call" ! recorderOk ^
+              "call recorder raises exception if we try to fetch a call when none was made" ! recorderFailure
+  
+  def stubNoArgs = {
+    val stub = new Stub[Foo]
+    val expecting = stub.expecting(Expectation({_.foo}, "result"))
+    expecting.stubObject.foo must_== "result"
+  }
+  
+  def recorderOk = {
+    val recorder = new Recorder[Foo]
+    recorder().foo
+    recorder.methodCalled should_== "foo"
+  }
+  
+  def recorderFailure = {
+    val recorder = new Recorder[Foo]
+    recorder.methodCalled should throwA[IllegalStateException].like { 
+      case e => e.getMessage must beMatching("Expecting a call on proxy object of class '.*Foo' but none was made.".r)  
+    } 
+      
+  }
+  
 }
