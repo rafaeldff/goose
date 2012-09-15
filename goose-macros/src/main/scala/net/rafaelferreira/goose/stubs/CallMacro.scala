5288c2c2ba3813bc38876ae26bd25a93fb238166
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-09-15 20:27:20 -0300

    Call.capture capturing argument values.

--- goose-macros/src/main/scala/net/rafaelferreira/goose/stubs/CallMacro.scala
+++ goose-macros/src/main/scala/net/rafaelferreira/goose/stubs/CallMacro.scala
@@ -8,9 +8,33 @@ import scala.language.experimental.macros
 object CallMacro {
   def capture_impl[T: c.AbsTypeTag](c: Context)(methodCall: c.Expr[T => Any]): c.Expr[Call] = {
     import c.universe._
-    val Function(_, Select(_, methodTermName)) = methodCall.tree  
+    val (methodTermName, values) = methodCall.tree match {
+      case Function(_, Select(_, methodTermName)) => (methodTermName, None)  
+      case Function(_, Apply(Select(_, methodTermName), termValues)) => (methodTermName, Some(termValues))
+      case _ => c.abort(c.enclosingPosition, "Expression %s is not a simple method call" format methodCall)
+    }
     val methodNameTree: Tree = Literal(Constant(methodTermName.toString))
     val methodNameExpression = c.Expr[String](methodNameTree)
-    reify( new net.rafaelferreira.goose.stubs.Call(methodNameExpression.splice, Nil) )
+    
+    values match {
+      case None => reify( new Call(methodNameExpression.splice, Nil) )
+      case Some(valuesTrees) =>
+        val valueListTree = 
+          Apply(
+            Select(
+              Select(Select(Select(Ident("scala"), newTermName("collection")), newTermName("immutable")), newTermName("List")),
+              newTermName("apply")),
+            valuesTrees)
+                  
+        val valueListExpression = c.Expr[List[Any]](valueListTree)
+        reify(new Call(methodNameExpression.splice, valueListExpression.splice))
+    }
   }   
 }
+
+
+/*
+Function(List(ValDef(Modifiers(PARAM | SYNTHETIC), newTermName("x$1"), Ident(Foo), EmptyTree)), 
+Apply(Select(Typed(Ident(newTermName("x$1")), Ident(Foo)), newTermName("oneArg")), List(Literal(Constant(9)))))
+
+*/
--- src/test/scala/net/rafaelferreira/goose/stubs/CallSpec.scala
+++ src/test/scala/net/rafaelferreira/goose/stubs/CallSpec.scala
@@ -6,9 +6,41 @@ import org.specs2.Specification
 class CallSpec extends Specification {
   trait Dummy {
     def noArgs:String
+    def oneIntArg(i:Int)
+    def twoArgs(i:Int,s:String)
   }
-  def is = {
-   val call = Call.capture[Dummy](_.noArgs)
-   call.method must_== "noArgs"
+  
+  def is = "Capturing" ^
+    "no-arguments method" ! noArguments ^
+    "method that takes one integer argument" ! argumentTakingInt ^
+    "method that takes two arguments" ! takingTwoArguments ^
+    "method whose closure takes an explicit parameter list" ! explicitParams ^
+    "method whose closure takes an explicit typed parameter list" ! explicitTypedParams
+  
+  def noArguments = {
+    val call = Call.capture[Dummy](_.noArgs)
+    call.method must_== "noArgs"
   }
+  
+  def argumentTakingInt = {
+    val call = Call.capture[Dummy](_.oneIntArg(42))
+    call must_== Call("oneIntArg", Seq(42))
+  }
+  
+  
+  def takingTwoArguments = {
+    val call = Call.capture[Dummy](_.twoArgs(42, "text"))
+    call must_== Call("twoArgs", Seq(42, "text")) 
+  }
+  
+  def explicitParams = {
+    val call = Call.capture[Dummy]((x) => x.oneIntArg(42))
+    call must_== Call("oneIntArg", Seq(42))
+  }
+  
+  def explicitTypedParams = {
+    val call = Call.capture((x:Dummy) => x.oneIntArg(42))
+    call must_== Call("oneIntArg", Seq(42))
+  }
+
 }
