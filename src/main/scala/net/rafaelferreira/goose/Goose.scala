b0caf6a08d2729bff5fef25a957795dd74899aa7
Rafael de F. Ferreira <public@rafaelferreira.net>
2012-09-30 17:24:26 -0300

    Extracting GooseStructure to its own file:
    it is the link between Goose's DSL to specs2

--- src/main/scala/net/rafaelferreira/goose/Goose.scala
+++ src/main/scala/net/rafaelferreira/goose/Goose.scala
@@ -1,53 +1,9 @@
 package net.rafaelferreira
 package goose
 
-import org.specs2
-import org.specs2.specification.Fragments
-import org.specs2.execute.Result
-import org.specs2.specification.Example
-import org.specs2.execute.DecoratedResult
-import org.specs2.Specification
-import org.specs2.matcher.MatchResult
-import org.specs2.specification.Fragment
-import org.specs2.execute.Failure
 import scala.reflect.ClassTag
 
-trait GooseStructure {this: Specification =>
-  import scala.collection.immutable.Map
-  
-  type ResultExpression[R] = Environment => Either[String,R]
-  
-  class When[R](resultExpression: ResultExpression[R], environment: Environment = new Environment(), val fragments:Fragments = Fragments()) {
-    def when[T](assumption: Assumption[T]): When[R] = copy(newState = environment.assuming(assumption)) 
-    def and[T](assumption: Assumption[T]): When[R] = when(assumption)
-    
-    def but(context: When[R] => When[R]): When[R] = {
-      val subWhen = context(this)
-      copy(newFragments = subWhen.fragments)
-    }
-    
-    def then(expectedExpression: R => MatchResult[Any]): When[R] = {
-      val result = resultExpression(environment)
-      val thisExample = result match {
-        case Left(failureMessafe) => eg { Failure(failureMessafe) }
-        case Right(resultValue) => eg { (expectedExpression(resultValue)) }
-      }
-      
-      this.copy(newFragments = fragments add thisExample)
-    }
-    
-    def results: Fragments = fragments ^ end
-    
-    private def copy(newResultExpression: ResultExpression[R] = resultExpression, newState: Environment = environment, newFragments:Fragments = fragments) =
-      new When(newResultExpression, newState, newFragments)
-    
-  }
-  
-  type Dependency[T] <: GeneralDependency[T]
-  def newDependency[T: ClassTag](name:String): Dependency[T]
-}
-
-trait Goose extends GooseStructure with CheckingForVariousArities with stubs.Stubs with direct.Direct {self: Specification =>
+trait Goose extends GooseStructure with CheckingForVariousArities with stubs.Stubs with direct.Direct {
   class ActualDependency[T: ClassTag](name:String) extends GeneralDependency[T] with DirectDependency[T] with StubDependency[T] {self =>
     override def toString = "DEP[%s]" format name
   }
---
+++ src/main/scala/net/rafaelferreira/goose/GooseStructure.scala
@@ -0,0 +1,44 @@
+package net.rafaelferreira
+package goose
+
+import scala.reflect.ClassTag
+import org.specs2.specification.Fragments
+import org.specs2.Specification
+import org.specs2.matcher.MatchResult
+import org.specs2.execute.Failure
+
+
+trait GooseStructure extends Specification {
+  import scala.collection.immutable.Map
+  
+  type ResultExpression[R] = Environment => Either[String,R]
+  
+  class When[R](resultExpression: ResultExpression[R], environment: Environment = new Environment(), val fragments:Fragments = Fragments()) {
+    def when[T](assumption: Assumption[T]): When[R] = copy(newState = environment.assuming(assumption)) 
+    def and[T](assumption: Assumption[T]): When[R] = when(assumption)
+    
+    def but(context: When[R] => When[R]): When[R] = {
+      val subWhen = context(this)
+      copy(newFragments = subWhen.fragments)
+    }
+    
+    def then(expectedExpression: R => MatchResult[Any]): When[R] = {
+      val result = resultExpression(environment)
+      val thisExample = result match {
+        case Left(failureMessafe) => eg { Failure(failureMessafe) }
+        case Right(resultValue) => eg { (expectedExpression(resultValue)) }
+      }
+      
+      this.copy(newFragments = fragments add thisExample)
+    }
+    
+    def results: Fragments = fragments ^ end
+    
+    private def copy(newResultExpression: ResultExpression[R] = resultExpression, newState: Environment = environment, newFragments:Fragments = fragments) =
+      new When(newResultExpression, newState, newFragments)
+    
+  }
+  
+  type Dependency[T] <: GeneralDependency[T]
+  def newDependency[T: ClassTag](name:String): Dependency[T]
+}
